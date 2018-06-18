bd649f012206f2eaaba935a9337422c5e31eaff8
Nick Taylor <nicholasjt22@gmail.com>
2018-06-18 13:03:21 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Styles.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <ul>
+          <li>PLACE_HOLDER</li>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Brands</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Header;

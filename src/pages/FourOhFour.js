2f37a0728b3b8e9fec6efb54af238956da6e96d5
Nick Taylor <nicholasjt22@gmail.com>
2018-06-18 14:04:24 -0700

    Added 404 page

---
+++ src/pages/FourOhFour.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const FourOhFour = function() {
+  return (
+    <div className="Page">
+      <Header />
+      You done goofed son, this page isnt a page.
+    </div>
+  );
+};
+
+export default FourOhFour;

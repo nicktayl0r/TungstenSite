6e971af4a47f87d5525962c1e0be11d962294afc
Nick Taylor <nicholasjt22@gmail.com>
2018-06-20 16:58:51 -0700

    contact form content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,6 +5,12 @@ const Contact = function() {
   return (
     <div className="Page">
       <Header />This is the Contact Page.
+      <form id="form" class="topBefore">
+        <input id="name" type="text" placeholder="NAME">
+        <input id="email" type="text" placeholder="E-MAIL">
+        <textarea id="message" type="text" placeholder="MESSAGE"></textarea>
+        <input id="submit" type="submit" value="GO!">
+      </form>
     </div>
   );
 };

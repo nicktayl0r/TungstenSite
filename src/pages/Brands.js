e0478c8d255f0bd8905f109f812584e17cbea391
Nick Taylor <nicholasjt22@gmail.com>
2018-06-27 12:46:58 -0700

    contact div on brands page

--- src/pages/Brands.js
+++ src/pages/Brands.js
@@ -1,12 +1,37 @@
 import React from "react";
+import "./PageStyles.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Parallax } from "react-parallax";
 
 const Brands = function() {
   return (
     <div className="Page">
       <Header />
-      This is the Brands Page.
+      <Parallax bgImage={require("./../assets/wallpaper1.jpg")} strength={500}>
+        <div className="parallax p1" />
+      </Parallax>
+      <Parallax bgImage={require("./../assets/wallpaper2.jpg")}>
+        <div className="parallax p2">
+          <div>Lorem ipsum dolor</div>
+        </div>
+      </Parallax>
+      <Parallax bgImage={require("./../assets/wallpaper3.jpg")}>
+        <div className="parallax p3">
+          <div>Lorem ipsum dolor</div>
+          <div>sit amet, consectetur adipiscing elit</div>
+        </div>
+      </Parallax>
+      <Parallax bgImage={require("./../assets/wallpaper4.jpg")}>
+        <div className="parallax p4">
+          <div>Lorem ipsum dolor</div>
+          <div>sit amet, consectetur adipiscing elit</div>
+        </div>
+      </Parallax>
+      <div className="contactDiv">
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+        <div>Contact Us</div>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,27 +5,28 @@ import Footer from "../components/Footer";
 import { Parallax } from "react-parallax";
 
 const Home = function() {
-  const wp2 = "./../assets/wallpaper2.jpg";
-  const wp3 = "./../assets/wallpaper3.jpg";
-  const wp4 = "./../assets/wallpaper4.jpg";
-
   return (
     <div className="Page">
       <Header />
-
       <Parallax bgImage={require("./../assets/wallpaper1.jpg")} strength={500}>
-        <div className="parallax">Parallax</div>
+        <div className="parallax p1" />
       </Parallax>
       <Parallax bgImage={require("./../assets/wallpaper2.jpg")}>
-        <div className="parallax">Parallax</div>
+        <div className="parallax p2">
+          <div>Lorem ipsum dolor</div>
+        </div>
       </Parallax>
       <Parallax bgImage={require("./../assets/wallpaper3.jpg")}>
-        {" "}
-        <div className="parallax">Parallax</div>
+        <div className="parallax p3">
+          <div>Lorem ipsum dolor</div>
+          <div>sit amet, consectetur adipiscing elit</div>
+        </div>
       </Parallax>
       <Parallax bgImage={require("./../assets/wallpaper4.jpg")}>
-        {" "}
-        <div className="parallax">Parallax</div>
+        <div className="parallax p4">
+          <div>Lorem ipsum dolor</div>
+          <div>sit amet, consectetur adipiscing elit</div>
+        </div>
       </Parallax>
       <Footer />
     </div>
